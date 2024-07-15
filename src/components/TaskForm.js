9b54c48f45fd70fde13e8b1f1f3861f0b978bb5b
Grayson Leversha <167662816+ggleversha@users.noreply.github.com>
2024-07-15 12:09:28 +1200

    Create TaskForm.js

---
+++ src/components/TaskForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TaskForm.css';
+
+const TaskForm = ({ userId, storeTask, daysCompleted, taskSubmitted }) => {
+    return (
+        <form className="task-form">
+            {/* Form elements */}
+        </form>
+    );
+};
+
+export default TaskForm;
