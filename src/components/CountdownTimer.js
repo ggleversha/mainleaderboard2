2e8449096cb86e470dd40291de3ff4238ac786e6
Grayson Leversha <167662816+ggleversha@users.noreply.github.com>
2024-07-15 12:10:02 +1200

    Create CountdownTimer.js

---
+++ src/components/CountdownTimer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CountdownTimer.css';
+
+const CountdownTimer = ({ stopTimerForUser, onDayEnd }) => {
+    return (
+        <div className="countdown-timer">
+            {/* Timer display */}
+        </div>
+    );
+};
+
+export default CountdownTimer;
